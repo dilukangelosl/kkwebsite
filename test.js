3336a6408c51e3dd49fb9921b72147e67e1a1372
diluk angelo <dilukangelo@gmail.com>
2023-11-14 16:37:53 +0530

    changes  14th

---
+++ test.js
@@ -0,0 +1,7 @@
+function calculateDaysBetweenDates(begin, end) {
+    const oneDay = 24 * 60 * 60 * 1000; // hours * minutes * seconds * milliseconds
+    const firstDate = new Date(begin);
+    const secondDate = new Date(end);
+    const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
+    return diffDays;
+}
